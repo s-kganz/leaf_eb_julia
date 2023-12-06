50ce83a085d6795a5f46bf8f362c6318b5357a01
s-kganz <ganzkeenan1@gmail.com>
2023-12-06 12:57:20 -0800

    fix issues with units in data pipeline

--- download_data.R
+++ download_data.R
@@ -12,28 +12,39 @@ load("data/MR.Rdata")
 
 # Select important columns. These aren't really documented on Zenodo so I'm
 # guessing on what some of these mean.
-BCI <- BCI %>%
-  select(date, DOY, Tcan.full, Tcan.roi.evergreen, Tcan.roi.deciduous,
-         Tair, wind.speed, SWdn, LWdn, RH, GPP)
+BCI2 <- BCI %>%
+  select(date, Tair, wind.speed, SWdn, LWdn, RH, GPP) %>%
+  rename(datetime = date) %>%
+  mutate(RH = RH / 100,
+         Tair = Tair + 273,
+         date_str = lubridate::format_ISO8601(datetime, use_z_format=FALSE)) %>%
+  select(date_str, everything(), -datetime) %>%
+  drop_na()
 
-MR <- MR %>%
-  select(DateTime, NEE, Tair, RH, Pressure, WS, SW_IN, SW_OUT, LW_IN, LW_OUT)
+MR2 <- MR %>%
+  select(date, NEE, Tair, RH, Pressure, WS, SW_IN, SW_OUT, LW_IN, LW_OUT) %>%
+  rename(datetime=date) %>%
+  mutate(RH = RH / 100,
+         Tair = Tair + 273,
+         date_str = lubridate::format_ISO8601(datetime, use_z_format=FALSE)) %>%
+  select(date_str, everything(), -datetime) %>%
+  drop_na()
 
 # Standardize names
-names(BCI) <- c(
-  "date (:Date)", "DOY (day)", "Tair (°C)", "wind.speed (m / s)", "SWdn (W / m^2 / s)",
-  "LWdn (W m^-2 s^-1)", "RH (%)", "GPP (μmol / m^2 / s)"
+names(BCI2) <- c(
+  "date (:DateTime)", "Tair (K)", "wind_speed (m / s)", "SWdn (W / m^2)",
+  "LWdn (W / m^2)", "RH", "GPP (μmol / m^2 / s)"
 )
 
-names(MR) <- c(
-  "date (:Date)", "GPP (μmol / m^2 / s)", "Tair (°C)", "RH (%)", "Pressure (kPa)",
-  "wind.speed (m / s)", "SWdn (W / m^2 / s)", "SWup (W / m^2 / s)", "LWdn (W / m^2 / s)",
-  "LWup (W / m^2 / s)"
+names(MR2) <- c(
+  "date (:DateTime)", "GPP (μmol / m^2 / s)", "Tair (K)", "RH", "Pressure (kPa)",
+  "wind_speed (m / s)", "SWdn (W / m^2)", "SWup (W / m^2)", "LWdn (W / m^2)",
+  "LWup (W / m^2)"
 )
 
 # Write output
-write_csv(BCI, "data/BCI.csv")
-write_csv(MR, "data/MR.csv")
+write_csv(BCI2, "data/BCI.csv")
+write_csv(MR2, "data/MR.csv")
 
 # Delete rdata files
 system("bash -c 'rm -f data/BCI.Rdata'")
